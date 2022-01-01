4ffb36ec0c212c6064ab952705a97b31ee43f27b
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-01-01 15:35:05 +0330

    feat: update color palette

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,17 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import {createTheme} from '@mui/material/styles';
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+    palette: {
+        primary: {
+            main: "#B3BCD0", // simple grey
+            contrastText: "#525F7B" // grey text
+        },
+        secondary:{
+            main: "#020B44", //dark blue
+            contrastText: "#FFFFFF" // white text
+        }
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
 });
 
 export default theme;
