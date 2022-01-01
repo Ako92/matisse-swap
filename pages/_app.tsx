0ef68d2353b8bd5821b7ada9a3a8954a276bff0c
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-01-01 15:31:40 +0330

    fix: fix server side missed styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,13 @@ interface MyAppProps extends AppProps {
 }
 
 export default function MyApp(props: MyAppProps) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles?.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
