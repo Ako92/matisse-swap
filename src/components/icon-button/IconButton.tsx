44c5676a0d23e361aba9ee634af4347ab75b11df
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-01-02 12:55:52 +0330

    feat: add small render mode

--- src/components/icon-button/IconButton.tsx
+++ src/components/icon-button/IconButton.tsx
@@ -6,8 +6,11 @@ import {makeStyles} from '@mui/styles';
 type Props = {
     icon: string;
     onClick: () => void;
+    isSmall?: boolean;
 }
-
+type TStyles = {
+    isSmall: boolean
+}
 
 const useStyles = makeStyles({
     root: {
@@ -20,7 +23,7 @@ const useStyles = makeStyles({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 0,
-        padding: "20px",
+        padding: (props: TStyles) => props.isSmall ? "1px" : "20px",
         '&:hover': {
             backgroundColor: 'rgba(31, 109, 201, 0.2)',
             borderColor: 'none',
@@ -30,7 +33,7 @@ const useStyles = makeStyles({
 });
 
 export default function (props: Props & Omit<ButtonProps, keyof Props>) {
-    const classes = useStyles(props);
+    const classes = useStyles({isSmall: !!props.isSmall});
     return (
         <Button className={classes.root} variant='contained' onClick={props.onClick}>
             <img src={`/icons/${props.icon}.png`} alt={props.icon}/>
